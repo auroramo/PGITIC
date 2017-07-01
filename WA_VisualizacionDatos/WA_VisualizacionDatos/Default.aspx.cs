e8044f00c6aed849f5d7ef940179cbe49cea08af
auroramo <Aurora.Macias@alu.uclm.es>
2017-07-01 19:22:39 +0200

    Commit añadido controlador de la vista

--- WA_VisualizacionDatos/WA_VisualizacionDatos/Default.aspx.cs
+++ WA_VisualizacionDatos/WA_VisualizacionDatos/Default.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +10,60 @@ namespace WA_VisualizacionDatos
 {
     public partial class _Default : Page
     {
-        protected void Page_Load(object sender, EventArgs e)
+        ServiceReferenceDB.ServicioBDClient sr = new ServiceReferenceDB.ServicioBDClient();
+        private DataSet ds = new DataSet();
+
+        protected void Timer1_Tick(object sender, EventArgs e)
+
         {
+            CargarValores();
 
         }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            CargarValores();
+        }
+
+        private void CargarValores()
+        {
+            ds = sr.RecuperarLecturas();
+            if (ds.Tables.Count > 0)
+            {
+                GridView1.DataSource = ds.Tables[0];
+                GridView1.DataBind();
+            }
+        }
+
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                DataRowView drv = e.Row.DataItem as DataRowView;
+                if ((int)(drv[4]) <=4)
+                {
+                    e.Row.BackColor = System.Drawing.Color.Purple;
+
+                }
+                else if ((int)(drv[4]) > 4 && (int)(drv[4]) <= 14)
+                {
+                    e.Row.BackColor = System.Drawing.Color.CornflowerBlue;
+
+                }
+                else if ((int)(drv[4]) > 14 && (int)(drv[4]) <= 28)
+                {
+                    e.Row.BackColor = System.Drawing.Color.LemonChiffon;
+
+                }
+                else if ((int)(drv[4]) > 28)
+                {
+                    e.Row.BackColor = System.Drawing.Color.Salmon;
+
+                }
+
+            }
+        }
+
+
     }
 }
